9c9d6c049986fe96c96094b4095b7cf3a69da8ac
SUMANTA5 <sumanta111195@gmail.com>
2022-06-09 15:27:22 +0530

    wather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Weather from "./components/Weather";
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  let componentMounted = true;
+
+  const fetchData = async () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      console.log(lat);
+      console.log(long);
+    });
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=842118caf5e45f6d06bb1dcbf6d2854c`
+    );
+
+    if (componentMounted) {
+      setData(await response.json());
+      console.log(data);
+    }
+
+    return () => {
+      componentMounted = false;
+    };
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [lat, long]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  {(typeof data.main != 'undefined') ? (
+    <Weather weatherData= {data}/>
+  ): (
+   <div>Loding....</div>
+  )
+  }
+  </>
   );
 }
 
---
+++ src/components/Weather.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Weather() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div class="card text-white bg-primary mb-3">
+              <div class="card-header">Weather</div>
+              <div class="card-body">
+                <h5 class="card-title">Primary card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Weather;
